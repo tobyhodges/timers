f2fabe4477242d761770c3f5a4a79218af461b02
tobyhodges <tbyhdgs@gmail.com>
2016-08-01 11:44:32 +0200

    reorganised clitimer.py.

--- clitimer.py
+++ clitimer.py
@@ -8,29 +8,40 @@ Created on Fri Jul 29 16:31:08 2016
 from sys import argv, stdout
 from time import sleep
 
-try:
-    input_time = argv[1]
-except IndexError:
-    input_time = input('Please provide a length of time to countdown (seconds or mm:ss):  ')
-
-if ':' in input_time:
-    minutes, seconds = input_time.split(':')
-    minutes = int(minutes)
-    seconds = int(seconds)
-    start_time = remaining_time = minutes*60 + seconds
-else:
-    start_time = remaining_time = int(input_time)
-
-remaining_mins = int(remaining_time/60)
-remaining_secs = int(remaining_time%60)
-stdout.write('%02d:%02d' % (remaining_mins, remaining_secs))
-while remaining_time > 0:
-    sleep(1)
-    remaining_time -= 1
-    remaining_mins = int(remaining_time/60)
-    remaining_secs = int(remaining_time%60)
+# Functions
+def display_time(rtime):
+    remaining_mins = int(rtime/60)
+    remaining_secs = int(rtime%60)
     stdout.write('\u001b[5D%02d:%02d' % (remaining_mins, remaining_secs))
     stdout.flush()
 
-stdout.write('\u001b[5D00:00\a\n')
+def countdown(rtime):
+    while rtime > 0:
+        sleep(1)
+        rtime -= 1
+        display_time(rtime)
+
+def run_timer(stime):
+    display_time(stime)
+    countdown(stime)
+    stdout.write('\u001b[5D00:00\a\n')
+
+if __name__ == "__main__":
+# Get input time, or prompt user for input if argument not provided
+    try:
+        input_time = argv[1]
+    except IndexError:
+        input_time = input(
+        'Please provide a length of time to countdown (seconds or mm:ss):  ')
+# Process input time
+    if ':' in input_time:
+        minutes, seconds = input_time.split(':')
+        minutes = int(minutes)
+        seconds = int(seconds)
+        start_time = minutes*60 + seconds
+    else:
+        start_time = int(input_time)
     
+# run countdown
+    run_timer(start_time)
+        
